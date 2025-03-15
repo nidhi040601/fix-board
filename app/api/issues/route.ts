f7284fd2d39a8f9b234346412359098d504b981b
Nidhi Sharma <you@example.com>
2025-03-15 17:02:33 -0400

    feat: Handle error and client-side validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,20 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
+import { createIssueSchema } from "../../validationSchemas";
 
 const prisma = new PrismaClient();
-
-const createIssueSchema = z.object({
-  title: z.string().min(1).max(200),
-  description: z.string().min(1),
-});
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
   const validation = createIssueSchema.safeParse(body); //safeParse doesn't throw errors if validation fails instead returns a object
   if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
   }
 
   const newIssue = await prisma.issue.create({
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,41 +2,65 @@
 
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-type IssueForm = {
-  title: string;
-  description: string;
-};
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, control, handleSubmit, formState } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-4"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root placeholder="Title" {...register("title")}>
-        <TextField.Slot />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description.." {...field} />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-4"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("An unexpected error occured");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")}>
+          <TextField.Slot />
+        </TextField.Root>
+        {formState.errors.title && (
+          <Text color="red" as="p">
+            {formState.errors.title.message}
+          </Text>
         )}
-      />
-      <Button>Submit New Issue</Button>
-    </form>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description.." {...field} />
+          )}
+        />
+        {formState.errors.description && (
+          <Text color="red" as="p">
+            {formState.errors.description.message}
+          </Text>
+        )}
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 };
 
