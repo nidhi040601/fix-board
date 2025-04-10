2abe00782b177d67f76384a1215505bd6917833b
Nidhi Sharma <you@example.com>
2025-04-10 16:33:51 -0400

    feat: Fix filter to consider other params

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -3,7 +3,7 @@
 import { Select } from "@radix-ui/themes";
 import { issueStatus } from "@/app/lib/issueStatusUtils";
 import React from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const statuses: {
   label: string;
@@ -13,11 +13,16 @@ const statuses: {
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
       onValueChange={(status) => {
-        const query = status != "ALL" ? `?status=${status}` : "";
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+        const query = params.size ? "?" + params.toString() : "";
         router.push("/issues/list" + query);
       }}
     >
